8c93907d6bc26d02f968bb9bb6801ac3d4992a8b
Sindre Sorhus <sindresorhus@gmail.com>
2018-12-23 00:25:13 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -8,16 +8,12 @@ export interface Options {
 }
 
 /**
+ * Create a clickable link in the terminal
+ *
  * [Supported terminals.](https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda)
- *
  * For unsupported terminals, the link will be printed in parens after the text: `My website (https://sindresorhus.com)`.
- *
- * @param text
- * @param url
- * @param options
- * @returns The string ready for output.
  */
-export default function terminalLink(text:string, url: string, options?: Options): string;
+export default function terminalLink(text: string, url: string, options?: Options): string;
 
 /**
  * Check whether the terminal support links.
