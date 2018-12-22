da0ddd7bbd4abac8a7903e9e481637f71af81ce8
Sindre Sorhus <sindresorhus@gmail.com>
2018-12-23 00:20:44 +0100

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,8 +1,10 @@
 import {expectType} from 'tsd-check';
-import terminalLink from '.';
-import {isSupported} from '.';
+import terminalLink, {isSupported} from '.';
 
-expectType<string>(terminalLink("text", "url"));
-expectType<string>(terminalLink("text", "url", {fallback: (text, url)=> `[${text}](${url})`}));
+expectType<string>(terminalLink('text', 'url'));
+
+expectType<string>(terminalLink('text', 'url', {
+	fallback: (text, url) => `[${text}](${url})`
+}));
 
 expectType<boolean>(isSupported);
