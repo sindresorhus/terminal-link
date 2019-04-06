a649623a8b35b62553b36e41017d700db8095249
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-06 11:18:58 +0000

    Refactor TypeScript definition to CommonJS compatible export (#6)

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,43 @@
-export interface Options {
-	/**
-	 * Override the default fallback.
-	 *
-	 * @default `${text} (${url})`
-	 */
-	fallback?: (text: string, url: string) => string;
+declare namespace terminalLink {
+	interface Options {
+		/**
+		Override the default fallback.
+
+		@default `${text} (${url})`
+		*/
+		fallback?: (text: string, url: string) => string;
+	}
 }
 
-/**
- * Create a clickable link in the terminal.
- *
- * [Supported terminals.](https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda)
- * For unsupported terminals, the link will be printed in parens after the text: `My website (https://sindresorhus.com)`.
- */
-export default function terminalLink(text: string, url: string, options?: Options): string;
+declare const terminalLink: {
+	/**
+	Create a clickable link in the terminal.
 
-/**
- * Check whether the terminal support links.
- *
- * Prefer just using the default fallback or the `fallback` option whenever possible.
- */
-export const isSupported: boolean;
+	[Supported terminals.](https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda)
+	For unsupported terminals, the link will be printed in parens after the text: `My website (https://sindresorhus.com)`.
+
+	@param text - Text to linkify.
+	@param url - URL to link to.
+
+	@example
+	```
+	import terminalLink = require('terminal-link');
+
+	const link = terminalLink('My Website', 'https://sindresorhus.com');
+	console.log(link);
+	```
+	*/
+	(text: string, url: string, options?: terminalLink.Options): string;
+
+	/**
+	Check whether the terminal support links.
+
+	Prefer just using the default fallback or the `fallback` option whenever possible.
+	*/
+	readonly isSupported: boolean;
+
+	// TODO: Remove this for the next major release
+	default: typeof terminalLink;
+};
+
+export = terminalLink;
