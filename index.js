8f9fe43518446719a87a3b6cfca89862e5211242
Tom Sherman <the.tomsherman@gmail.com>
2019-06-10 12:16:29 +0100

    Add stderr

--- index.js
+++ index.js
@@ -2,14 +2,19 @@
 const ansiEscapes = require('ansi-escapes');
 const supportsHyperlinks = require('supports-hyperlinks');
 
-module.exports = (text, url, options = {}) => {
-	if (!supportsHyperlinks.stdout) {
+const terminalLink = (text, url, options = {}, target = 'stdout') => {
+	if (!supportsHyperlinks[target]) {
 		return options.fallback ? options.fallback(text, url) : `${text} (${url})`;
 	}
 
 	return ansiEscapes.link(text, url);
 };
 
+module.exports = (text, url, options = {}) => terminalLink(text, url, options);
+
+module.exports.stderr = (text, url, options = {}) => terminalLink(text, url, options, 'stderr');
+
 // TODO: Remove this for the next major release
 module.exports.default = module.exports;
 module.exports.isSupported = supportsHyperlinks.stdout;
+module.exports.stderr.isSupported = supportsHyperlinks.stderr;
--- test.js
+++ test.js
@@ -18,11 +18,29 @@ test('main', t => {
 	t.is(actual, '\u001B]8;;https://sindresorhus.com\u0007My Website\u001B]8;;\u0007');
 });
 
+test('stderr', t => {
+	process.env.FORCE_HYPERLINK = 1;
+	const m = require('.');
+
+	const actual = m.stderr('My Website', 'https://sindresorhus.com');
+	console.log(actual);
+	t.is(actual, '\u001B]8;;https://sindresorhus.com\u0007My Website\u001B]8;;\u0007');
+});
+
 test('default fallback', t => {
 	process.env.FORCE_HYPERLINK = 0;
 	const m = require('.');
 
 	const actual = m('My Website', 'https://sindresorhus.com');
+	console.log(actual);
+	t.is(actual, 'My Website (https://sindresorhus.com)');
+});
+
+test('stderr default fallback', t => {
+	process.env.FORCE_HYPERLINK = 0;
+	const m = require('.');
+
+	const actual = m.stderr('My Website', 'https://sindresorhus.com');
 	console.log(actual);
 	t.is(actual, 'My Website (https://sindresorhus.com)');
 });
@@ -38,7 +56,23 @@ test('custom fallback', t => {
 	t.is(actual, 'My Website: https://sindresorhus.com');
 });
 
+test('custom fallback stderr', t => {
+	process.env.FORCE_HYPERLINK = 0;
+	const m = require('.');
+
+	const actual = m.stderr('My Website', 'https://sindresorhus.com', {
+		fallback: (text, url) => `${text}: ${url}`
+	});
+	console.log(actual);
+	t.is(actual, 'My Website: https://sindresorhus.com');
+});
+
 test('isSupported', t => {
 	const m = require('.');
 	t.is(typeof m.isSupported, 'boolean');
+});
+
+test('isSupported stderr', t => {
+	const m = require('.');
+	t.is(typeof m.stderr.isSupported, 'boolean');
 });
