1d248b6247703aa5de31efad79341619f24db98c
Tom Sherman <the.tomsherman@gmail.com>
2019-09-01 05:57:10 +0100

    Add stderr support (#9)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.js
+++ index.js
@@ -2,14 +2,19 @@
 const ansiEscapes = require('ansi-escapes');
 const supportsHyperlinks = require('supports-hyperlinks');
 
-module.exports = (text, url, options = {}) => {
-	if (!supportsHyperlinks.stdout) {
+const terminalLink = (text, url, {target = 'stdout', ...options} = {}) => {
+	if (!supportsHyperlinks[target]) {
 		return options.fallback ? options.fallback(text, url) : `${text} (\u200B${url}\u200B)`;
 	}
 
 	return ansiEscapes.link(text, url);
 };
 
+module.exports = (text, url, options = {}) => terminalLink(text, url, options);
+
+module.exports.stderr = (text, url, options = {}) => terminalLink(text, url, {target: 'stderr', ...options});
+
 // TODO: Remove this for the next major release
 module.exports.default = module.exports;
 module.exports.isSupported = supportsHyperlinks.stdout;
+module.exports.stderr.isSupported = supportsHyperlinks.stderr;
