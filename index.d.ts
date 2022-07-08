8311a11c5ab95c7c7bf4983c565276759c86fc31
Sindre Sorhus <sindresorhus@gmail.com>
2022-07-08 15:12:41 +0200

    Meta tweaks

--- index.d.ts
+++ index.d.ts
@@ -8,41 +8,6 @@ export interface Options {
 }
 
 declare const terminalLink: {
-	readonly stderr: {
-		/**
-		Check whether the terminal's stderr supports links.
-
-		Prefer just using the default fallback or the `fallback` option whenever possible.
-		*/
-		readonly isSupported: boolean;
-
-		/**
-		Create a clickable link in the terminal's stderr.
-
-		[Supported terminals.](https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda)
-		For unsupported terminals, the link will be printed in parens after the text: `My website (https://sindresorhus.com)`.
-
-		@param text - Text to linkify.
-		@param url - URL to link to.
-
-		@example
-		```
-		import terminalLink from 'terminal-link';
-
-		const link = terminalLink.stderr('My Website', 'https://sindresorhus.com');
-		console.error(link);
-		```
-		*/
-		(text: string, url: string, options?: Options): string;
-	};
-
-	/**
-	Check whether the terminal supports links.
-
-	Prefer just using the default fallback or the `fallback` option whenever possible.
-	*/
-	readonly isSupported: boolean;
-
 	/**
 	Create a clickable link in the terminal's stdout.
 
@@ -62,6 +27,41 @@ declare const terminalLink: {
 	```
 	*/
 	(text: string, url: string, options?: Options): string;
+
+	readonly stderr: {
+		/**
+		Create a clickable link in the terminal's stderr.
+
+		[Supported terminals.](https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda)
+		For unsupported terminals, the link will be printed in parens after the text: `My website (https://sindresorhus.com)`.
+
+		@param text - Text to linkify.
+		@param url - URL to link to.
+
+		@example
+		```
+		import terminalLink from 'terminal-link';
+
+		const link = terminalLink.stderr('My Website', 'https://sindresorhus.com');
+		console.error(link);
+		```
+		*/
+		(text: string, url: string, options?: Options): string;
+
+		/**
+		Check whether the terminal's stderr supports links.
+
+		Prefer just using the default fallback or the `fallback` option whenever possible.
+		*/
+		readonly isSupported: boolean;
+	};
+
+	/**
+	Check whether the terminal supports links.
+
+	Prefer just using the default fallback or the `fallback` option whenever possible.
+	*/
+	readonly isSupported: boolean;
 };
 
 export default terminalLink;
